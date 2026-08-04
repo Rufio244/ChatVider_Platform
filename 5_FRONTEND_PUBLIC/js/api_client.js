1d25ee9e1fbc128bfe6875f9d90f4e4129135da8
Rufio244 <thanvas9991@gmail.com>
2026-08-04 11:45:22 +0700

    Add async apiRequest function for API calls

--- 5_FRONTEND_PUBLIC/js/api_client.js
+++ 5_FRONTEND_PUBLIC/js/api_client.js
@@ -1,1 +1,8 @@
+async function apiRequest(path, options={}) {
+  const token = localStorage.getItem('vid_auth_token');
+  return fetch(`${API_BASE}${path}`, {
+    ...options,
+    headers: { 'Content-Type':'application/json', Authorization:`Bearer ${token}`, ...options.headers }
+  }).then(r => r.json());
+}
 
