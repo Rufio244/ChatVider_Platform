b78f47401a4d8c9f2e737642fec2b2a3553864a9
Rufio244 <thanvas9991@gmail.com>
2026-08-04 11:26:25 +0700

    Implement W2_UserUnderstanding class for user patterns

--- 1_CORE_FOUNDATION/W2_UserUnderstanding.js
+++ 1_CORE_FOUNDATION/W2_UserUnderstanding.js
@@ -1,1 +1,20 @@
+class W2_UserUnderstanding {
+  constructor() { this.userPatterns = {}; }
+
+  learnPattern(userId, input, style) {
+    if(!this.userPatterns[userId]) this.userPatterns[userId] = { inputs:[], preferences:{}, style:{} };
+    this.userPatterns[userId].inputs.push({text:input, time:new Date()});
+    Object.assign(this.userPatterns[userId].style, style);
+  }
+
+  getProfile(userId) { return this.userPatterns[userId] || null; }
+
+  adaptResponse(userId, baseText) {
+    const profile = this.getProfile(userId);
+    if(!profile) return baseText;
+    if(profile.style.format === "short") return baseText.substring(0,150)+"...";
+    return baseText;
+  }
+}
+module.exports = W2_UserUnderstanding;
 
