5a849d4b717de8e92a01b198861baffddcddfa05
Rufio244 <thanvas9991@gmail.com>
2026-08-04 11:50:38 +0700

    Add payment configuration settings

--- 7_BILLING_SUBSCRIPTION/payment_config.js
+++ 7_BILLING_SUBSCRIPTION/payment_config.js
@@ -1,1 +1,39 @@
+require('dotenv').config();
+
+const PAYMENT_CONFIG = {
+  owner: "Thanva Phupingbut 244",
+  
+  channels: {
+    promptpay: {
+      type: "PROMPTPAY",
+      target: "1529900399939",
+      name: "นายธันวา ภูปิงบุตร"
+    },
+    banks: {
+      kbank: { code: "0318613826", name: "กสิกรไทย" },
+      scb: { code: "4078670710", name: "ไทยพาณิชย์" }
+    },
+    stripe: {
+      secretKey: process.env.STRIPE_SECRET_KEY || "",
+      currency: "thb"
+    }
+  },
+
+  settings: {
+    autoActivate: true,
+    graceDays: 3,
+    trialDays: 7,
+    defaultPlan: "free",
+    currency: "THB",
+    allowPartial: false
+  },
+
+  limits: {
+    maxDiscountPercent: 30,
+    minTopupAmount: 100,
+    taxRatePercent: 7
+  }
+};
+
+module.exports = PAYMENT_CONFIG;
 
